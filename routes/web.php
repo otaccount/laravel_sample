7a5527f20563ed1e7ffa122019d5216dd574cf96
oota <oota@LAPTOP-RA3F3PRM.localdomain>
2022-09-23 22:17:31 +0900

    add login

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/test', 'App\Http\Controllers\TestController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
